f29f96af54ca0c060e478f4fb386ebc940679fc9
AliMughal41 <alimghal41@gmail.com>
2025-04-29 21:35:41 +0500

    complete chat  support functionality added

---
+++ backend/routes/chatRouter.js
@@ -0,0 +1,10 @@
+// chatRouter.js
+import express from "express";
+import { saveMessage, getChatMessages } from "../controllers/chatController.js"; // Correct import
+
+const router = express.Router();
+
+router.post("/send", saveMessage);  // Route for sending messages
+router.get("/messages/:senderId/:receiverId", getChatMessages);  // Route for getting messages
+
+export default router;
